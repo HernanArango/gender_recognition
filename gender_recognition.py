7475c9b9b5673adda8d3e8acd67e002c2d8731b2
hernan <hernan@localhost.localdomain>
2018-05-01 00:56:16 -0500

    implementado reconocimiento de genero en video por reconocimiento de rostro

--- gender_recognition.py
+++ gender_recognition.py
@@ -1,29 +1,14 @@
-from local_binary_patterns import LocalBinaryPatterns
-from sklearn.svm import LinearSVC
-from imutils import paths
-import pickle
-#opencv
-import cv2
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from recognition_lib import *
+import argparse
+
  
-# initialize the local binary patterns descriptor along with
-# the data and label lists
-desc = LocalBinaryPatterns(24, 8)
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--camera", action="store_true")
+args = ap.parse_args()
 
-
-model = pickle.load(open("model.save", 'rb'))
-
-
-# loop over the testing images
-for imagePath in paths.list_images("faces/testing"):
-	# load the image, convert it to grayscale, describe it,
-	# and classify it
-	image = cv2.imread(imagePath)
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	hist = desc.describe(gray)
-	prediction = model.predict([hist])[0]
- 
-	# display the image and the prediction
-	cv2.putText(image, prediction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
-		1.0, (0, 0, 255), 3)
-	cv2.imshow("Image", image)
-	cv2.waitKey(0)
+if args.camera:
+	recognition_camera()
+else:
+	recognition_images()
